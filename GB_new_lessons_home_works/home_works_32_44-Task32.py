e7cd61b6a327e0ccfdfc9f85a884037d37f0fb6b
DanielFalcony <DanielFalcony@gmail.com>
2022-08-31 16:27:24 +0300

    Tasks 11 - 31 Done Tasks 32 - 44
    HM Lesson 4 Task 1 Done

---
+++ GB_new_lessons_home_works/home_works_32_44-Task32.py
@@ -0,0 +1,29 @@
+"""
+Task 31
+
+Составить список простых множителей натурального числа N
+
+
+"""
+
+
+def simple_multiplier(i):
+    n = i
+    multi_list = [n]
+    while n != 1:
+        if n % 2 == 0:
+            multi_list.append(n // 2)
+            n = n // 2
+        elif n % 3 == 0:
+            multi_list.append(n // 3)
+            n = n // 3
+        else:
+            multi_list.append(n // n)
+            n = n // n
+
+    return print(f'простые множители числа {i} это {multi_list}')
+
+
+number = int(input('Введите число: '))
+
+print(simple_multiplier(number))
