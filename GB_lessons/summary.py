c5b58a7d1f0d89a2e8b05a583397bb22baaa6dae
DanielFalcony <DanielFalcony@gmail.com>
2022-09-09 10:20:44 +0300

    Sum programm done

---
+++ GB_lessons/summary.py
@@ -0,0 +1,5 @@
+from input_values import param_1, param_2
+from summary_method import summary_method
+
+
+print(summary_method(param_1, param_2))
---
+++ GB_lessons/summary_method.py
@@ -0,0 +1,4 @@
+def summary_method(a, b):
+    result = a + b
+    return f'Сумма чесел {a} и {b} равна {result}'
+
