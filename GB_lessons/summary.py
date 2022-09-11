83a02318b921b5c9261a572d07259adae518d48d
Daniil <danielfalcony@gmail.com>
2022-09-11 17:01:40 +0300

    mini_calc

--- GB_lessons/summary.py
+++
@@ -1,5 +0,0 @@
-from input_values import param_1, param_2
-from summary_method import summary_method
-
-
-print(summary_method(param_1, param_2))
--- GB_lessons/summary_method.py
+++
@@ -1,4 +0,0 @@
-def summary_method(a, b):
-    result = a + b
-    return f'Сумма чесел {a} и {b} равна {result}'
-
